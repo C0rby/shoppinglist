53f5d3b2e740b605d55012c71d7aadd94d845a2b
David Christofas <dchristofas@posteo.de>
2022-06-11 01:05:45 +0200

    continue redesign of the app
    implemented new design of the checkboxes and reimplemented the delete feature

--- web/shoppinglist-ui/src/components/ShoppingListEntries.vue
+++ web/shoppinglist-ui/src/components/ShoppingListEntries.vue
@@ -58,6 +58,19 @@ const updateEntry = (listId: string, entry: entry) => {
     );
 }
 
+const deleteEntry = (listId: string, entry: entry) => {
+    fetch(
+        import.meta.env.VITE_BACKEND_URL +
+        "/api/v1/shoppinglists/" +
+        listId +
+        "/entries/" +
+        entry.id,
+        {
+            method: "DELETE",
+        }
+    );
+}
+
 const filteredResults = computed(() => {
     if (!search.value) {
         return listEntries.value
@@ -116,18 +129,6 @@ watch(() => store.state.selectedList, (val) => {
 
 
 
-const deleteEntry = (listId, entry) => {
-    fetch(
-        import.meta.env.VITE_BACKEND_URL +
-        "/api/v1/shoppinglists/" +
-        listId +
-        "/entries/" +
-        entry.id,
-        {
-            method: "DELETE",
-        }
-    );
-}
 
 const clearSearch = () => {
     search.value = "";
@@ -137,36 +138,62 @@ const clearSearch = () => {
 */
 </script>
 <template>
-    <div class="flex items-center bg-gray-200 rounded-md mt-4">
+    <div class="flex items-center bg-gray-200 rounded-md mt-4 mx-2 md:mx-1">
         <div class="pl-2">
             <svg class="fill-current text-gray-500 w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                 <path class="heroicon-ui"
                     d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
             </svg>
         </div>
-        <input class="w-full rounded-md bg-gray-200 text-gray-700 leading-tight focus:outline-none py-2 px-2"
-            id="search" type="text" placeholder="Search or add" />
+        <input
+            class="w-full rounded-md bg-gray-200 text-gray-700 leading-tight focus:outline-none py-2 px-2 border-0 focus:ring-0 ring-0"
+            id="search" type="text" placeholder="Search or add" v-model="search" />
     </div>
-    <div class="mt-4 text-sm flex flex-col flex-auto h-0 overflow-y-scroll">
+    <div class="mt-4 text-sm flex flex-col flex-auto h-0 overflow-y-scroll px-2 md:px-1">
         <div v-for="entry in filteredResults" :key="entry.id"
-            class="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
+            class="flex justify-start cursor-pointer text-gray-700 my-4 first:mt-0 last:mb-0 items-center">
             <label class="flex flex-grow justify-start items-start">
-                <div
-                    class="bg-white border-2 rounded border-gray-400 w-6 h-6 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-emerald-500">
-                    <input type="checkbox" class="opacity-0 absolute" :checked="!entry.buy"
-                        @change="updateEntry(props.selectedlistId, entry)" />
-                    <svg class="fill-current hidden w-4 h-4 text-emerald-500 pointer-events-none" viewBox="0 0 20 20">
-                        <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
-                    </svg>
-                </div>
-                <div class="flex-grow font-medium px-2">{{ entry.name }}</div>
+                <input type="checkbox"
+                    class="w-8 h-8 rounded-full transition-colors duration-400 focus:outline-0 focus:ring-0 focus:ring-offset-0"
+                    :class="{ 'text-emerald-500': !entry.buy }" :checked="!entry.buy"
+                    @change="updateEntry(props.selectedlistId, entry)" />
+                <div class="flex-grow font-bold text-xl ml-2" :class="{ 'text-gray-300 line-through': !entry.buy }">{{
+                        entry.name
+                }}</div>
             </label>
-            <div class="text-sm font-normal text-gray-500 tracking-wide">{{ entry.amount }}</div>
+            <div class=" font-normal text-gray-500 mr-2">{{ entry.amount }}</div>
+            <div class="dropdown">
+                <button class="text-gray-500 focus:outline-none h-7 flex items-center" type="button"
+                    aria-haspopup="true" aria-expanded="true" aria-controls="headlessui-menu-items-117">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="text-gray-500 h-6 w-6" fill="none"
+                        viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z" />
+                    </svg>
+                </button>
+                <div
+                    class="opacity-0 invisible dropdown-menu transition-all duration-300 transform origin-top-right -translate-y-2 scale-95">
+                    <div class="absolute right-0 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none"
+                        aria-labelledby="headlessui-menu-button-1" id="headlessui-menu-items-117" role="menu">
+                        <div class="py-1">
+                            <a href="javascript:void(0)" @click="deleteEntry(props.selectedlistId, entry)" tabindex="0"
+                                class="text-gray-700 flex items-center text-lg px-4 py-2" role="menuitem"><svg
+                                    xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
+                                    viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                </svg> Delete</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 <style>
-input:checked+svg {
-    display: block;
+.dropdown:focus-within .dropdown-menu {
+    opacity: 1;
+    transform: translate(0) scale(1);
+    visibility: visible;
 }
 </style>
